46161be0c93e207e5447b1091ca055ea14692aa4
Wolfgang Werner <w.werner@vhmhv.de>
2018-10-24 01:41:47 +0200

    Fix active navigation item

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,13 +1,17 @@
 <template>
   <nav class="navbar is-transparent  is-fixed-top">
-      <div id="navbarMenu" class="navbar-menu is-active">
-        <div class="navbar-end">
-          <div class="tabs is-right">
-            <ul>
-              <li class="is-active"><router-link to="/">Generator</router-link></li>
-              <li><router-link to="about">About</router-link></li>
-            </ul>
-            <span class="navbar-item">
+    <div id="navbarMenu" class="navbar-menu is-active">
+      <div class="navbar-end">
+        <div class="tabs is-right">
+          <ul>
+            <li :class="{ 'is-active' : $route.path === '/' }">
+              <router-link to="/">Generator</router-link>
+            </li>
+            <li :class="{ 'is-active' : $route.path === '/about' }">
+              <router-link to="about">About</router-link>
+            </li>
+          </ul>
+          <span class="navbar-item">
                 <a class="button is-dark is-outlined"
                    href="https://github.com/wwerner/basherplate">
                     <span>Source</span>
@@ -16,9 +20,15 @@
                     </span>
                 </a>
             </span>
-          </div>
         </div>
       </div>
+    </div>
   </nav>
 </template>
+
+<script>
+export default {
+  name: 'Navigation',
+};
+</script>
 
