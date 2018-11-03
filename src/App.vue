7f37b8b89a470ed4104a9f3cc4fe63711c6c38a4
Wolfgang Werner <w.werner@vhmhv.de>
2018-11-03 15:00:20 +0100

    Move made with to footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,19 @@
   <div id="app" class="section">
     <navigation />
     <router-view></router-view>
+    <footer class="footer box">
+      <div class="content has-text-centered">
+        Bash Plate is standing on the shoulders of these giants:
+          <a href="https://vuejs.org/">Vue.js</a>,
+          <a href="https://bulma.io/">Bulma</a>,
+          <a href="https://prismjs.com/">Prism.js</a>,
+          <a href="https://handlebarsjs.com/">handlebars</a>.
+        Thanks to
+          <a href="https://github.com">Github</a> for VCS,
+          <a href="https://zeit.co">Zeit</a> for hosting,
+          <a href="https://circleci.com">CircleCI</a> for CI/CD.
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -19,5 +32,10 @@ export default {
 <style>
 #app {
 
+}
+
+.footer {
+  background-color: transparent;
+  padding: 0.3rem 0.3rem 0.3rem 0.3rem;
 }
 </style>
