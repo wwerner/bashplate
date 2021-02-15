84d165528189dcef6d0589a8ad510fa4a2241fc2
Wolfgang Werner <mail@wolfgang-werner.net>
2021-02-15 21:50:28 +0100

    Change styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,27 @@ import * as React from "react";
 
 export const Footer = () => (
     <footer className="footer box">
-            <div className="content has-text-centered">
-                Bash Plate is standing on the shoulders of giants:
-                    &nbsp;<a href="https://reactjs.org/">React.js</a>,
-                    &nbsp;<a href="https://bulma.io/">Bulma</a>,
-                    &nbsp;<a href="https://prismjs.com/">Prism.js</a>,
-                    &nbsp;<a href="https://handlebarsjs.com/">handlebars</a>.
-                Thanks to
-                    &nbsp;<a href="https://github.com">Github</a> for VCS,
-                    &nbsp;<a href="https://vercel.com">Vercel</a> for hosting,
-                    &nbsp;<a href="https://circleci.com">CircleCI</a> for CI/CD.
-             </div>
-        </footer>
+        <div className="content has-text-centered">
+            Bashplate is standing on the shoulders of giants:
+            {' '}
+            <a href="https://reactjs.org/"><i className='fab fa-react' style={{ color: "gray" }}></i> React.js</a>,
+            {' '}
+            <a href="https://bulma.io/">Bulma</a>,
+            {' '}
+            <a href="https://prismjs.com/"><i className='fa fa-caret-up' style={{ color: "gray" }}></i> Prism.js</a>,
+            {' '}
+            <a href="https://handlebarsjs.com/">handlebars</a>,
+            {' '}
+            <a href="https://github.com"><i className='fab fa-github' style={{ color: "gray" }}></i> Github</a>
+            {' '}
+            and <a href="https://vercel.com">Vercel</a>.
+            {' '}
+            Built with 
+            {' '}
+            <i className='fa fa-heart' style={{ color: "hotpink" }}></i> 
+             
+            {' '}
+            by <a href='https://wolfgang-werner.net'>this guy</a>.
+        </div>
+    </footer>
 )
