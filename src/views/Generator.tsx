97822c3e7da2da2ef8f70e3341a0cb9026231b5a
Wolfgang Werner <mail@wolfgang-werner.net>
2021-02-22 20:47:40 +0100

    Test pre-commit lint&fix

--- src/views/Generator.tsx
+++ src/views/Generator.tsx
@@ -5,9 +5,9 @@ import { OptionData, ResultData } from "~model";
 type GeneratorProps = {
   options: OptionData[];
   result: ResultData;
-     onAddOption: () => void;
-onChangeOption: (option: OptionData) => void;
-            onRemoveOption: (option: OptionData) => void;
+onAddOption: () => void;
+    onChangeOption: (option: OptionData) => void;
+  onRemoveOption: (option: OptionData) => void;
   onChangeDialect: any;
 };
 
